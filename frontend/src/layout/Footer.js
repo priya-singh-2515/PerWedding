b567f725d55a9ef0bfa7995752d7cfb995b2b48c
tarun <chauhantarun803@gmail.com>
2024-03-22 12:05:05 +0530

    changes

--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -3,9 +3,28 @@ import Footertop from "./Footertop";
 
 const Footer = () => {
   return (
-    <div>
+    <>
       <Footertop />
-    </div>
+      <div className="container px-5 text-center pt-3 pb-5">
+        <div className="row px-5">
+          <span className="footerspan">Copyright 2024</span>
+          <span className="footerspan">
+            Lin and Jirsa | Los Angeles Wedding Photographer
+          </span>
+          <div className="col-sm-12">
+            <ul className="footerul">
+              <li>Resources</li>
+              <li>Vandors</li>
+              <li>OC Venues</li>
+              <li>LA Venues</li>
+              <li>Tutorials</li>
+              <li>Jobs</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../images/Logo.jpg";
-
+import "./home.css";
 import "react-image-gallery/styles/css/image-gallery.css";
 import Navbar from "../layout/Navbar";
 // import Footertop from "../layout/Footertop";
@@ -8,45 +8,72 @@ import FirstComp from "../slideComp/FirstComp";
 import Footer from "../layout/Footer";
 
 const Home = () => {
+  const style = {
+    backgroundImage: `url("https://i.vimeocdn.com/video/721056442-5d3959de48e444c17d20f83fd86b9d35871458d5c5ba97a722d4c74e99d47143-d?mw=2000&mh=838&q=70")`,
+    backgroundSize: "cover",
+  };
+  const image = [
+    { img: "./images/feat_logo1.png" },
+    { img: "./images/feat_logo2.png" },
+    { img: "./images/feat_logo4.png" },
+    { img: "./images/feat_logo7.png" },
+    { img: "./images/canon.png" },
+    { img: "./images/la.png" },
+  ];
+
   return (
     <>
       <Navbar />
-      <div className="container-fluid">
+      <div style={style} className="container-fluid tophome py-5">
         <div className="row">
-          <div style={{ height: "90vh", width: "100%" }} className="col-lg-12">
-            <iframe
-              title="vimeo-player"
-              src="https://player.vimeo.com/video/286209377?h=5115aa7873"
-              width="100%"
-              height="100%"
-              frameborder="0"
-              allowfullscreen
-            ></iframe>
+          <div className="col-lg-12">
+            <h1>LOS ANGELES WEDDING PHOTOGRAPHER</h1>
           </div>
         </div>
       </div>
 
-      <div className="container-fluid">
+      <div className="container py-5">
         <div className="row">
-          <div className="col-lg-12">
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <img
-                src={logo}
-                alt={logo}
-                style={{ height: "15vh", width: "20%", textAlign: "center" }}
-              />
-              <div>
-                <p>
-                  If you're looking for a Los Angeles Wedding Photographer, then
-                  you're in the right place! Since 2008, Lin and Jirsa has set
-                  the standard for creative wedding photography. Our style is
-                  deeply rooted in wedding photojournalism, influenced by
-                  fine-art and fashion photography. The result is creative,
-                  clean and crisp wedding photography that’s romantic, fun, and
-                  emotional. We pride ourselves on not just being photographers,
-                  but rather artists creating unique and expressive imagery.
-                </p>
-              </div>
+          {/* <div className="col-lg-12"> */}
+          <div
+            className="col-lg-12 py-4"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <img
+              src={logo}
+              alt="logo"
+              style={{ height: "10vh", width: "20%" }}
+            />
+          </div>
+
+          {/* </div> */}
+        </div>
+        <div className="row">
+          <div className="col-lg-12 logotext">
+            <p>
+              If you're looking for a Los Angeles Wedding Photographer, then
+              you're in the right place! Since 2008, Lin and Jirsa has set the
+              standard for creative wedding photography. Our style is deeply
+              rooted in wedding photojournalism, influenced by fine-art and
+              fashion photography. The result is creative, clean and crisp
+              wedding photography that’s romantic, fun, and emotional. We pride
+              ourselves on not just being photographers, but rather artists
+              creating unique and expressive imagery.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{ backgroundColor: "#1a1a1a63" }}
+        className="container px-5 py-5 text-center"
+      >
+        <p className="text-light">FEATURED IN:</p>
+        <div className="row px-5 ">
+          <div className="col-sm-12 px-5">
+            <div className="featmap">
+              {image.map((e) => (
+                <img src={e.img} />
+              ))}
             </div>
           </div>
         </div>
