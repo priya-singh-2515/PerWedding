b2b84f9c743a50f3524387158854f8340eaddd90
priya-singh-2515 <priyasingh_coder@hotmail.com>
2024-03-22 12:36:51 +0530

    services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import WeddingPhotography from "./pages/services/WeddingPhotography";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services/weddingphotography" element={<WeddingPhotography/>}/>
       </Routes>
     </>
   );
